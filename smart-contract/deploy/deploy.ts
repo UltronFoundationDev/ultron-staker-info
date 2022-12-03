a2220ca081b713fe620730bf2b2deace527e16a7
ultron <software@ultron.foundation>
2022-12-03 04:36:28 +0200

    refactor(Sergej): tasks

--- smart-contract/deploy/deploy.ts
+++ smart-contract/deploy/deploy.ts
@@ -1,4 +1,3 @@
-import { getDefaultProvider } from '@ethersproject/providers';
 import { subtask, task, types } from "hardhat/config";
 import * as Helpers from './helpers';
 import * as fs from 'fs';
@@ -6,32 +5,29 @@ import * as fs from 'fs';
 const fileName: string = 'wallets.txt';
 
 task("deploy", "Deploy StakerInfo")
-  .setAction(async (taskArgs, {ethers}) => {
-        const signer = (await ethers.getSigners())[0];
+  .setAction(async (taskArgs, hre) => {
+        const signer = (await hre.ethers.getSigners())[0];
 
         const sfc = '0xfc00face00000000000000000000000000000000';
 
-        const stakerInfoFactory = await ethers.getContractFactory("StakerInfo", signer);
+        const stakerInfoFactory = await hre.ethers.getContractFactory("StakerInfo", signer);
         const stakerInfo = await (await stakerInfoFactory.deploy(sfc)).deployed();
 
         console.log("StakerInfo deployed to:", stakerInfo.address);
     });
 
 task("update-info", "Updating cfg info")
-    .setAction(async (taskArgs, {ethers, network}) => {
+    .setAction(async (taskArgs, hre) => {
         let stakerInfoAddress = '';
-        let ethersProvider = getDefaultProvider();
+        let ethersProvider = hre.ethers.provider;
         let rootPath = ''
 
-        if(network.name === "ultron") {
+        if(hre.network.name === "ultron") {
             stakerInfoAddress = '0x8346c42d1023BAfA955fF3623c96d54982AB8b0F';
-            //TODO use hardhat config
-            ethersProvider = new ethers.providers.JsonRpcProvider("https://ultron-rpc.net");
             rootPath = 'https://raw.githubusercontent.com/UltronFoundationDev/ultron-staker-info/main/configs'
         }
-        else if(network.name === "ultron_testnet") {
+        else if(hre.network.name === "ultron_testnet") {
             stakerInfoAddress = '0x33F0C573e9415497D30FB7C1bd4632b2F27dC689';
-            ethersProvider = new ethers.providers.JsonRpcProvider("https://ultron-dev.io");
             rootPath = 'https://raw.githubusercontent.com/UltronFoundationDev/ultron-staker-info/main/configs/testnet'
         }
 
@@ -42,11 +38,11 @@ task("update-info", "Updating cfg info")
                 continue;
             }
 
-            let wallet = new ethers.Wallet(validators[i - 1]);
+            let wallet = new hre.ethers.Wallet(validators[i - 1]);
 
             const walletSigner = wallet.connect(ethersProvider);
 
-            let stakerInfo = await ethers.getContractAt("StakerInfo", stakerInfoAddress, walletSigner);
+            let stakerInfo = await hre.ethers.getContractAt("StakerInfo", stakerInfoAddress, walletSigner);
 
             let cfgUrl = `${rootPath}/Validator${i}.json`;
             // console.log(cfgUrl);
@@ -58,38 +54,34 @@ task("update-info", "Updating cfg info")
 
 task("get-info", "Get validator cfg info")
     .addParam("id", "Validator id")
-    .setAction(async (taskArgs, {ethers, network}) => {
+    .setAction(async (taskArgs, hre) => {
 
         let stakerInfoAddress = '';
-        let ethersProvider = getDefaultProvider();
-        if(network.name === "ultron") {
+        if(hre.network.name === "ultron") {
             stakerInfoAddress = '0x8346c42d1023BAfA955fF3623c96d54982AB8b0F';
-            //TODO use hardhat config
-            ethersProvider = new ethers.providers.JsonRpcProvider("https://ultron-rpc.net");
         }
-        else if(network.name === "ultron_testnet") {
+        else if(hre.network.name === "ultron_testnet") {
             stakerInfoAddress = '0x33F0C573e9415497D30FB7C1bd4632b2F27dC689';
-            ethersProvider = new ethers.providers.JsonRpcProvider("https://ultron-dev.io");
         }
 
-        let stakerInfo = await ethers.getContractAt("StakerInfo", stakerInfoAddress);
+        let stakerInfo = await hre.ethers.getContractAt("StakerInfo", stakerInfoAddress);
 
         console.log(`Validator id ${taskArgs.id}:`, await stakerInfo.getInfo(taskArgs.id))
     });
 
 task("change-owner", "Transfer ownership")
-    .setAction(async (taskArgs, {ethers, network}) => {
-        const signer = (await ethers.getSigners())[0];
+    .setAction(async (taskArgs, hre) => {
+        const signer = (await hre.ethers.getSigners())[0];
 
         let stakerInfoAddress = '';
-        if(network.name === "ultron") {
+        if(hre.network.name === "ultron") {
             stakerInfoAddress = '0x8346c42d1023BAfA955fF3623c96d54982AB8b0F';
         }
-        else if(network.name === "ultron_testnet") {
+        else if(hre.network.name === "ultron_testnet") {
             stakerInfoAddress = '0x33F0C573e9415497D30FB7C1bd4632b2F27dC689';
         }
         
-        const stakerInfo = await ethers.getContractAt("StakerInfo", stakerInfoAddress, signer);
+        const stakerInfo = await hre.ethers.getContractAt("StakerInfo", stakerInfoAddress, signer);
 
         const owner = '0x4CE535D6E2D47690e33CA646972807BeB264dFBf';
 
@@ -99,16 +91,16 @@ task("change-owner", "Transfer ownership")
     });
 
 task("update-reward", "updateBaseRewardPerSecond")
-  .setAction(async (taskArgs, {ethers}) => {
-        const signer = (await ethers.getSigners())[0];
+  .setAction(async (taskArgs, hre) => {
+        const signer = (await hre.ethers.getSigners())[0];
 
         const sfcAddress = '0xfc00face00000000000000000000000000000000';
-        const sfc = await ethers.getContractAt("SFC", sfcAddress, signer);
+        const sfc = await hre.ethers.getContractAt("SFC", sfcAddress, signer);
         let perSecond = await sfc.baseRewardPerSecond();
         console.log(`Before perSecond: ${perSecond}`);
         
-        const newPerSecond = ethers.utils.parseEther("1");
-        await sfc.updateBaseRewardPerSecond(newPerSecond);
+        // const newPerSecond = hre.ethers.utils.parseEther("1");
+        // await sfc.updateBaseRewardPerSecond(newPerSecond);
 
         await Helpers.delay(2000);
         perSecond = await sfc.baseRewardPerSecond();
